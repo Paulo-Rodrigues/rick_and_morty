54e5b93a123fc2d59e0ca0f6cdddf0ba5c366d99
Paulo Rodrigues <paulor2222@gmail.com>
2020-01-01 13:17:54 -0200

    add headers

--- lib/rick_and_morty.rb
+++ lib/rick_and_morty.rb
@@ -2,13 +2,23 @@ require "rick_and_morty/version"
 require 'httparty'
 
 module RickAndMorty
-  class RickAndmorty
+  class Ricked
     include HTTParty
 
     base_uri("rickandmortyapi.com/api/")
 
     def characters
-      self.class.get('character/')
+      self.class.get('character/', headers: headers)
+    end
+
+    def character(id)
+      self.class.get("character/#{id}", headers: headers)
+    end
+
+    private
+
+    def headers
+      {"Accept" => "application/json"}
     end
   end
 end
