cefce35ffccaa286ee01cf3d6dd85bc584671484
Paulo Rodrigues <paulor2222@gmail.com>
2020-01-04 19:42:16 -0200

    loc returns a hash

--- spec/lib/locations_spec.rb
+++ spec/lib/locations_spec.rb
@@ -2,8 +2,11 @@ require 'spec_helper'
 
 RSpec.describe RickAndMorty::Locations do
   describe 'returns array of locations' do
-    xit 'list locatioons' do
-
+    it 'list locatioons' do
+      loc = RickAndMorty::Locations.new
+      puts "#############"
+      puts loc.locations.class
+      expect(loc.locations).to be_kind_of(Array)
     end
   end
 end
